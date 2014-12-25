8d9d12acf99ec35d4ecfecbc996653e4ae6b3ffc
Tiago Mergulhão <me@tmergulhao.com>
2014-12-25 13:32:36 -0200

    Initial Commit

---
+++ STNewsFeedParser/STNewsFeedParser.h
@@ -0,0 +1,19 @@
+//
+//  STNewsFeedParser.h
+//  STNewsFeedParser
+//
+//  Created by Tiago Mergulhão on 25/12/14.
+//  Copyright (c) 2014 Tiago Mergulhão. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STNewsFeedParser.
+FOUNDATION_EXPORT double STNewsFeedParserVersionNumber;
+
+//! Project version string for STNewsFeedParser.
+FOUNDATION_EXPORT const unsigned char STNewsFeedParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STNewsFeedParser/PublicHeader.h>
+
+
